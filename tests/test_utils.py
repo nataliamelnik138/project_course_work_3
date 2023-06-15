474a58ee3459a3f067b19fc9380a99591560c019
Natalia Melnik <nataliamelnik138@gmail.com>
2023-06-15 13:41:07 +0600

    Added a test for the account number masking function

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,4 @@
-from src.utils import converts_date_format, masks_card_number
+from src.utils import converts_date_format, masks_card_number, masks_account_number
 
 
 def test_converts_date_format():
@@ -8,3 +8,7 @@ def test_converts_date_format():
 def test_masks_card_number():
     assert masks_card_number("Visa Classic 6831982476737658") == "Visa Classic 6831 98** **** 7658"
     assert masks_card_number("MasterCard 7158300734726758") == "MasterCard 7158 30** **** 6758"
+
+
+def test_masks_account_number():
+    assert masks_account_number("Счет 10848359769870775355") == "Счет **5355"
