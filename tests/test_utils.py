3ecdf92ee5f29d05ed5cd89c95eced8960b3c545
Natalia Melnik <nataliamelnik138@gmail.com>
2023-06-15 11:12:21 +0600

    Created a test for a function that masks the card number

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,10 @@
-from src.utils import converts_date_format
+from src.utils import converts_date_format, masks_card_number
 
 
 def test_converts_date_format():
     assert converts_date_format("2018-03-23T10:45:06.972075") == "23.03.2018"
+
+
+def test_masks_card_number():
+    assert masks_card_number("Visa Classic 6831982476737658") == "Visa Classic 6831 98** **** 7658"
+    assert masks_card_number("MasterCard 7158300734726758") == "MasterCard 7158 30** **** 6758"
